126c41ea9e118709b3dd1b0df46faf543550c562
noslwop <marvin@frobnitz.co.uk>
2014-05-10 17:02:12 +0100

    Added viola.py file to setup.py script

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
 
 
+
 setup(name='Viola',
       version='0.8',
       description='A Blorb-capable Z-Machine interpreter.',
       author='David Fillmore',
       author_email='marvin@frobnitz.co.uk',
       packages=["zio", "zcode"],
+      scripts=['viola.py',],      
       py_modules=["babel", "blorb", "iff", "quetzal", "settings"],
       data_files=[('fonts', ['fonts/FreeMono.ttf',
                              'fonts/FreeMonoBold.ttf',
