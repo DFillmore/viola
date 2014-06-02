fbb65fd96e3041270a25c17e8fbba073acf4b50f
noslwop <marvin@frobnitz.co.uk>
2014-06-02 20:38:09 +0100

    Added standard check for number of sound channels.

--- zcode/sounds.py
+++ zcode/sounds.py
@@ -32,9 +32,13 @@ def setup(b):
     global AVAILABLE, blorbs
     try:
         pygame.mixer.init()
-        for a in range(io.maxeffectschannels()):
+        if zcode.use_standard >= STANDARD_12:
+            for a in range(io.maxeffectschannels()):
+                soundchannels[0].append(effectsChannel(a))
+            for a in range(io.maxmusicchannels()):
+                soundchannels[1].append(musicChannel(a))
+        else:
             soundchannels[0].append(effectsChannel(a))
-        for a in range(io.maxmusicchannels()):
             soundchannels[1].append(musicChannel(a))
         AVAILABLE = True
     except:
