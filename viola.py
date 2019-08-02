99f68a92c77182c1fc86c1bcab2f9397f0b27594
marvin <marvin@frobnitz.co.uk>
2019-08-02 14:45:43 +0100

    Fixed io module import

--- viola.py
+++ viola.py
@@ -14,7 +14,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-import zio.kivy as io
+import zio.io as io
 import sys
 import getopt
 import os
