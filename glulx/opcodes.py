68c7913f21d6a6113b17573492009b4b398e8f68
DFillmore <marvin@frobnitz.co.uk>
2023-09-30 23:49:07 +0100

    Added dummy function for opcodes

---
+++ glulx/opcodes.py
@@ -0,0 +1,449 @@
+def nop():
+    pass
+
+def add():
+    pass
+
+def sub():
+    pass
+
+def mul():
+    pass
+
+def div():
+    pass
+
+def mod():
+    pass
+
+def neg():
+    pass
+
+def bitand():
+    pass
+
+def bitor():
+    pass
+
+def bitxor():
+    pass
+
+def bitnot():
+    pass
+
+def shiftl():
+    pass
+
+def sshiftr():
+    pass
+
+def ushiftr():
+    pass
+
+def jump():
+    pass
+
+def jz():
+    pass
+
+def jnz():
+    pass
+
+def jeq():
+    pass
+
+def jne():
+    pass
+
+def jlt():
+    pass
+
+def jge():
+    pass
+
+def jgt():
+    pass
+
+def jle():
+    pass
+
+def jltu():
+    pass
+
+def jgeu():
+    pass
+
+def jgtu():
+    pass
+
+def jleu():
+    pass
+
+def call():
+    pass
+
+def return():
+    pass
+
+def catch():
+    pass
+
+def throw():
+    pass
+
+def tailcall():
+    pass
+
+def copy():
+    pass
+
+def copys():
+    pass
+
+def copyb():
+    pass
+
+def sexs():
+    pass
+
+def sexb():
+    pass
+
+def aload():
+    pass
+
+def aloads():
+    pass
+
+def aloadb():
+    pass
+
+def aloadbit():
+    pass
+
+def astore():
+    pass
+
+def astores():
+    pass
+
+def astoreb():
+    pass
+
+def astorebit():
+    pass
+
+def stkcount():
+    pass
+
+def stkpeek():
+    pass
+
+def stkswap():
+    pass
+
+def stkroll():
+    pass
+
+def stkcopy():
+    pass
+
+def streamchar():
+    pass
+
+def streamnum():
+    pass
+
+def streamstr():
+    pass
+
+def streamunichar():
+    pass
+
+def gestalt():
+    pass
+
+def debugtrap():
+    pass
+
+def getmemsize():
+    pass
+
+def setmemsize():
+    pass
+
+def jumpabs():
+    pass
+
+def random():
+    pass
+
+def setrandom():
+    pass
+
+def quit():
+    pass
+
+def verify():
+    pass
+
+def restart():
+    pass
+
+def save():
+    pass
+
+def restore():
+    pass
+
+def saveundo():
+    pass
+
+def restoreundo():
+    pass
+
+def protect():
+    pass
+
+def hasundo():
+    pass
+
+def discardundo():
+    pass
+
+def glk():
+    pass
+
+def getstringtbl():
+    pass
+
+def setstringtbl():
+    pass
+
+def getiosys():
+    pass
+
+def setiosys():
+    pass
+
+def linearsearch():
+    pass
+
+def binarysearch():
+    pass
+
+def linkedsearch():
+    pass
+
+def callf():
+    pass
+
+def callfi():
+    pass
+
+def callfii():
+    pass
+
+def callfiii():
+    pass
+
+def mzero():
+    pass
+
+def mcopy():
+    pass
+
+def malloc():
+    pass
+
+def mfree():
+    pass
+
+def accelfunc():
+    pass
+
+def accelparam():
+    pass
+
+def numtof():
+    pass
+
+def ftonumz():
+    pass
+
+def ftonumn():
+    pass
+
+def ceil():
+    pass
+
+def floor():
+    pass
+
+def fadd():
+    pass
+
+def fsub():
+    pass
+
+def fmul():
+    pass
+
+def fdiv():
+    pass
+
+def fmod():
+    pass
+
+def sqrt():
+    pass
+
+def exp():
+    pass
+
+def log():
+    pass
+
+def pow():
+    pass
+
+def sin():
+    pass
+
+def cos():
+    pass
+
+def tan():
+    pass
+
+def asin():
+    pass
+
+def acos():
+    pass
+
+def atan():
+    pass
+
+def atan2():
+    pass
+
+def jfeq():
+    pass
+
+def jfne():
+    pass
+
+def jflt():
+    pass
+
+def jfle():
+    pass
+
+def jfgt():
+    pass
+
+def jfge():
+    pass
+
+def jisnan():
+    pass
+
+def jisinf():
+    pass
+
+def numtod():
+    pass
+
+def dtonumz():
+    pass
+
+def dtonumn():
+    pass
+
+def ftod():
+    pass
+
+def dtof():
+    pass
+
+def dceil():
+    pass
+
+def dfloor():
+    pass
+
+def dadd():
+    pass
+
+def dsub():
+    pass
+
+def dmul():
+    pass
+
+def ddiv():
+    pass
+
+def dmodr():
+    pass
+
+def dmodq():
+    pass
+
+def dsqrt():
+    pass
+
+def dexp():
+    pass
+
+def dlog():
+    pass
+
+def dpow():
+    pass
+
+def dsin():
+    pass
+
+def dcos():
+    pass
+
+def dtan():
+    pass
+
+def dasin():
+    pass
+
+def dacos():
+    pass
+
+def datan():
+    pass
+
+def datan2():
+    pass
+
+def jdeq():
+    pass
+
+def jdne():
+    pass
+
+def jdlt():
+    pass
+
+def jdle():
+    pass
+
+def jdgt():
+    pass
+
+def jdge():
+    pass
+
+def jdisnan():
+    pass
+
+def jdisinf():
+    pass
