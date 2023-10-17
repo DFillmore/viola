26a3e3acf1ca6e797669f60815fe8323a5c1f7b5
DFillmore <marvin@frobnitz.co.uk>
2023-10-17 01:07:58 +0100

    fixed indentation

--- vio/zcode.py
+++ vio/zcode.py
@@ -65,17 +65,17 @@ def quit():
     sys.exit()
 
 def getBaseDir():
-   if getattr(sys,"frozen",False):
-       # If this is running in the context of a frozen (executable) file, 
-       # we return the path of the main application executable
-       return os.path.dirname(os.path.abspath(sys.executable))
-   else:
-       # If we are running in script or debug mode, we need 
-       # to inspect the currently executing frame. This enable us to always
-       # derive the directory of main.py no matter from where this function
-       # is being called
-       thisdir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-       return os.path.abspath(os.path.join(thisdir, os.pardir))
+    if getattr(sys,"frozen",False):
+        # If this is running in the context of a frozen (executable) file, 
+        # we return the path of the main application executable
+        return os.path.dirname(os.path.abspath(sys.executable))
+    else:
+        # If we are running in script or debug mode, we need 
+        # to inspect the currently executing frame. This enable us to always
+        # derive the directory of main.py no matter from where this function
+        # is being called
+        thisdir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+        return os.path.abspath(os.path.join(thisdir, os.pardir))
 
 def findfile(filename, gamefile=False):
     global GAMEDIRECTORY
